5904a2b5d2fdcfa49ead89782fa61342ef72654e
eliashead <eliashead.costa@gmail.com>
2022-08-18 20:08:29 -0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,36 @@ import { Post } from './components/Post'
 import './global.css'
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/eliashead.png',
+      name: 'Elias Costa',
+      role: 'CTO HeadTech'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu repositorio, É um projeto no NLW return, eventos da Rocketseat. O nome do projeto é DoctorCare.'},
+      {type: 'link', content:'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-08-17 17:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/oloko64.png',
+      name: 'Reinaldo',
+      role: 'CTO Simbiose'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu repositorio, É um projeto no NLW return, eventos da Rocketseat. O nome do projeto é DoctorCare.'},
+      {type: 'link', content:'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-08-10 18:00:00'),
+  },
+]
 export function App() {
   return (
     <div>
@@ -13,14 +43,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author='Elias Costa'
-            conteudo='O maior estadio do mundo'
-          />
-          <Post
-            author='Amanda Corado'
-            conteudo='Projeto PGNI'
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
